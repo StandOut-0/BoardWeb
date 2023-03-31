cedb5622cab5a4e3b8f7585a6d49054c144e55b4
StandOut <StandOut.first@gmail.com>
2023-03-31 12:06:14 +0900

    UserService 애너테이션 적용 컴포넌트 테스트

--- src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
+++ src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
@@ -1,9 +1,18 @@
 package com.springbook.biz.user.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.springbook.biz.user.UserService;
 import com.springbook.biz.user.UserVO;
 
+
+//public class UserServiceImpl implements UserService {
+	//private UserDAO userDAO;
+
+@Service("userService")
 public class UserServiceImpl implements UserService {
+	@Autowired
 	private UserDAO userDAO;
 
 	public void setUserDAO(UserDAO userDAO) {
