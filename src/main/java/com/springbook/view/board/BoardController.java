f33104e43e3f77a580e5db904d976f9e334064bb
StandOut <StandOut.first@gmail.com>
2023-04-11 11:19:50 +0900

    비즈니스 컴포넌트, VO DAO Service 구현하기 -  Web에서 Controller보다 먼저 구동되도록 ContextLoaderListener, context-param

--- src/main/java/com/springbook/view/board/BoardController.java
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -3,90 +3,67 @@ package com.springbook.view.board;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.ModelAndView;
 
+import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
 @Controller
 @SessionAttributes("board")
 public class BoardController {
+	@Autowired
+	private BoardService boardService;
 	
+	//컨트롤러에서 DAO메소드를 호출하면 안되는 이유는 유지보수 과정에서 DAO클래스를 다른클래스로 쉽게 교체하기 위해서다.
 	
 	// 글 등록
 	@RequestMapping(value = "/insertBoard.do")
-	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
-		boardDAO.insertBoard(vo);
+	public String insertBoard(BoardVO vo) {
+		boardService.insertBoard(vo);
 		return "getBoardList.do";
 	}
 
-	
-
 	// 글 수정
-//	@ModelAttribute("board")로 저장되는 데이터가 있다면
-//	@SessionAttributes("board") 세션에도 자동으로 저장하라. = null값으로 들어오지않음
 	@RequestMapping("/updateBoard.do")
-	public String updateBoard(@ModelAttribute("board") BoardVO vo, BoardDAO boardDAO) {
-		System.out.println("번호: "+ vo.getSeq());
-		System.out.println("제목: "+ vo.getTitle());
-		System.out.println("작성자: "+ vo.getWriter());
-		System.out.println("내용: "+ vo.getContent());
-		System.out.println("등록일: "+ vo.getRegDate());
-		System.out.println("조회수: "+ vo.getCnt());
-		boardDAO.updateBoard(vo);
+	public String updateBoard(@ModelAttribute("board") BoardVO vo) {			
+		boardService.updateBoard(vo);
 		return "getBoardList.do";
 	}
 
 	// 글 삭제
 	@RequestMapping("/deleteBoard.do")
-	public String deleteBoard(BoardVO vo, BoardDAO boardDAO) {
-		boardDAO.deleteBoard(vo);
+	public String deleteBoard(BoardVO vo) {
+		boardService.deleteBoard(vo);
 		return "getBoardList.do";
 	}
 
 	// 글 상세 조회
 	@RequestMapping("/getBoard.do")
-	public String getBoard(BoardVO vo, BoardDAO boardDAO, Model modal) {
-		modal.addAttribute("board", boardDAO.getBoard(vo)); // Model 정보 저장
-		return "getBoard.jsp";
+	public String getBoard(BoardVO vo, Model model) {
+		model.addAttribute("board", boardService.getBoard(vo)); // Model 정보 저장
+		return "getBoard.jsp"; // View 이름 리턴
 	}
 	
-	
-	//getBoardList가 실행되기전에 먼저실행된다.
+	// 검색 조건 목록 설정
 	@ModelAttribute("conditionMap")
-	public Map<String, String> SearchCondition(){
-		Map<String, String> conditionMap = new HashMap <String, String>();
-		conditionMap.put("제목","TITLE");
-		conditionMap.put("내용","CONTENT");
+	public Map<String, String> searchConditionMap(){
+		Map<String, String> conditionMap = new HashMap<String, String>();
+		conditionMap.put("제목", "TITLE");
+		conditionMap.put("내용", "CONTENT");
 		return conditionMap;
 	}
-	
 
 	// 글 목록 검색
-	//@RequestParam 
-	// value 화면에서 전달될 파라미터 이름
-	// defaultValue 전달될 파라미터 정보가 없을때 설정할 기본값
-	// required 생략여부
 	@RequestMapping("/getBoardList.do")
-	public String getBoardList(
-//			@RequestParam(value="searchCondition", defaultValue ="TITLE", required = false ) String condition,
-//			@RequestParam(value="searchKeyword", defaultValue ="", required = false ) String keyword,
-			BoardVO vo, BoardDAO boardDAO, Model modal) {
-		modal.addAttribute("boardList", boardDAO.getBoardList(vo)); // Model 정보 저장
-		
-//		String condition = vo.getSearchCondition();
-//		String keyword = vo.getSearchKeyword();
-//		System.out.println("검색조건"+condition);
-//		System.out.println("검색단어"+keyword);
-		
-		
-		
-		return "getBoardList.jsp";
+	public String getBoardList(BoardVO vo, Model model) {		
+		// Model 정보 저장
+		model.addAttribute("boardList", boardService.getBoardList(vo));																
+		return "getBoardList.jsp"; // View 이름 리턴
 	}
 }
